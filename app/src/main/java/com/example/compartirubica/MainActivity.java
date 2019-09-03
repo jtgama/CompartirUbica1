9aa5f739687267600bfab5d34c8e1e4ea2d5a725
jtgama <jhonatanralla@hotmail.com>
2019-09-02 23:21:35 -0500

    actualizacion1

--- app/src/main/java/com/example/compartirubica/MainActivity.java
+++ app/src/main/java/com/example/compartirubica/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.compartirubica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
+
 import android.os.Bundle;
 import android.view.View;
 
